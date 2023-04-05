8f2a03483576a547076555fa38b8b16fc01505d3
zoelin1122 <87543028+zoelin1122@users.noreply.github.com>
2023-04-05 09:07:43 -0400

    Fixed profile section

--- script.js
+++ script.js
@@ -105,6 +105,9 @@ window.addEventListener('scroll', () => {
   }
 });
 
+
+//Sticky Banners
+
 // const banner = document.querySelector('.banner');
 // const bannerOffsetTop = banner.offsetTop;
 
@@ -118,3 +121,112 @@ window.addEventListener('scroll', () => {
 //     banner.classList.remove('sticky');
 //   }
 // });
+
+
+
+// Set up the container and profiles
+const container = document.getElementById('dragableprofile-container');
+const profiles = document.querySelectorAll('.dragableprofile');
+
+// Set up variables for tracking drag events
+let isDragging = false;
+let startX, startY;
+let initialX, initialY;
+let currentX, currentY;
+
+
+// Add event listeners for drag events on each profile
+profiles.forEach(profile => {
+  profile.addEventListener('mousedown', dragStart);
+  profile.addEventListener('mouseup', dragEnd);
+  profile.addEventListener('mousemove', drag);
+});
+
+
+function dragStart(e) {
+  // Set up variables for tracking drag events
+  startX = e.clientX;
+  startY = e.clientY;
+  initialX = this.offsetLeft;
+  initialY = this.offsetTop;
+  
+  // Set isDragging flag to true
+  isDragging = true;
+
+  // Update line positions
+  updateLines();
+}
+
+function dragEnd(e) {
+  // Set isDragging flag to false
+  isDragging = false;
+  // Update line positions
+  updateLines();
+}
+
+function drag(e) {
+  if (isDragging) {
+    // Calculate the new position of the profile
+    currentX = e.clientX - startX;
+    currentY = e.clientY - startY;
+    
+    // Set the new position of the profile
+    this.style.left = (initialX + currentX) + 'px';
+    this.style.top = (initialY + currentY) + 'px';
+    updateLines();
+  }
+}
+
+function updateLines() {
+  // Get the line container and clear its contents
+  const lineContainer = document.getElementById('line-container');
+  lineContainer.innerHTML = '';
+
+  // Calculate the coordinates of each profile
+  const positions = [];
+  profiles.forEach(profile => {
+    const img = profile.getElementsByTagName('img')[0];
+    const x = profile.offsetLeft + img.offsetWidth / 2;
+    const y = profile.offsetTop + img.offsetHeight / 2;
+    positions.push({x: x, y: y});
+  });
+
+
+  // Draw a line between each pair of profiles
+  for (let i = 0; i < positions.length; i++) {
+    for (let j = i + 1; j < positions.length; j++) {
+      const line = document.createElement('div');
+      line.classList.add('line');
+      line.style.left = positions[i].x + 'px';
+      line.style.top = positions[i].y + 'px';
+      line.style.width = Math.sqrt(Math.pow(positions[j].x - positions[i].x, 2) + Math.pow(positions[j].y - positions[i].y, 2)) + 'px';
+      line.style.transformOrigin = 'left center';
+      line.style.transform = 'rotate(' + Math.atan2(positions[j].y - positions[i].y, positions[j].x - positions[i].x) + 'rad)';
+      lineContainer.appendChild(line);
+    }
+  }
+}
+
+// Initialize the profiles
+const profile1 = document.getElementById("profile1");
+const profile2 = document.getElementById("profile2");
+const profile3 = document.getElementById("profile3");
+const profile4 = document.getElementById("profile4");
+
+// Set the initial positions of the profiles
+profile1.style.left = "10px";
+profile1.style.top = "50px";
+profile2.style.left = "900px";
+profile2.style.top = "250px";
+profile3.style.left = "630px";
+profile3.style.top = "-30px";
+profile4.style.left = "300px";
+profile4.style.top = "300px";
+
+window.addEventListener('scroll', function() {
+  updateLines();
+});
+// Add event listener for window resize event
+window.addEventListener('scroll', updateLines);
+
+updateLines();
